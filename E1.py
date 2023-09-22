5b47c8b0a605e1bfc26e53068c06bbee91e2f070
LA2800 <145277543+LA2800@users.noreply.github.com>
2023-09-22 17:20:34 +0000

    Practica 1

---
+++ E1.py
@@ -0,0 +1,5 @@
+# solicita al usuario su nombre de usuario
+nombre_usuario= input ("Por favor, introduce tu nombre de usuario: ")
+
+# muestra un mensaje de saludo con el nombre del usuario
+print(f"Hola {nombre_usuario}")
