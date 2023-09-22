64773457e4e20f3809f0e3a3397f8178e0875ded
LA2800 <145277543+LA2800@users.noreply.github.com>
2023-09-22 17:44:08 +0000

    PC1

---
+++ E5.py
@@ -0,0 +1,3 @@
+cantidad_shows= int(input("Ingrese la cantidad de shows musicales que ha visto en el último año: "))
+ha_visto_mas_de_3 = cantidad_shows >3
+print(f"¿Ha visto más de 3 shows en el úlitmo año? {ha_visto_mas_de_3}")
