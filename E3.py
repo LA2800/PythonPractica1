5b17cabacb288d225fafe4e8b5e11a7ba98d4598
LA2800 <145277543+LA2800@users.noreply.github.com>
2023-09-22 17:34:54 +0000

    PC1

---
+++ E3.py
@@ -0,0 +1,6 @@
+peso_payaso =112
+peso_muñeca=75
+cantidad_payasos=int(input("Ingrese la cantidad de payasos vendidos: "))
+cantidad_muñecas = int(input("Ingrese la cantidad de muñecas vendidas: "))
+peso_total=(cantidad_payasos*peso_payaso)+(cantidad_muñecas*peso_muñeca)
+print (f"El peso total del paquete será {peso_total} Kg")
